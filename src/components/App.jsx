c11583d7b2cc147dfd9c798638fc44319f1d68f0
Yair <yair.facio11@gmail.com>
2020-05-28 09:31:54 -0500

    Passing app and book components to be stateless

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,13 @@ import React, { Component } from 'react';
 import BookList from '../containers/BooksList';
 import BookForm from '../containers/BooksForm';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <BookList />
-        <BookForm />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <BookList />
+      <BookForm />
+    </div>
+  );
 }
+
+export default App;
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Book extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { props: { book: { id, title, category }, handleRemoveBook } } = this;
-    const { book } = this.props;
-    return (
-      <tr>
-        <th>{id}</th>
-        <th>{title}</th>
-        <th>{category}</th>
-        <th>
-          <button onClick={() => handleRemoveBook(book)} type="button">Delete</button>
-        </th>
-      </tr>
-    );
-  }
+const Book = ({handleRemoveBook, book}) => {
+  const { id, title, category } = book;
+  return (
+    <tr>
+      <th>{id}</th>
+      <th>{title}</th>
+      <th>{category}</th>
+      <th>
+        <button onClick={() => handleRemoveBook(book)} type="button">Delete</button>
+      </th>
+    </tr>
+  );
 }
 
 Book.propTypes = {
   book: PropTypes.objectOf(PropTypes.string).isRequired,
   handleRemoveBook: PropTypes.func.isRequired,
 };
+
+export default Book;
